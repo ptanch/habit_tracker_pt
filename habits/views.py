847a1c730727ef59eb90e1777145633611e3f54a
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-23 22:50:15 +0300

    добавлены crud операции для модели habits через generics

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(CreateAPIView):
+    """API view для создания привычки"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitListAPIView(ListAPIView):
+    """API view для получения списка привычек"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitRetrieveAPIView(RetrieveAPIView):
+    """API view для получения детальной информации о привычке"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    """API view для обновления привычки"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitDestroyAPIView(DestroyAPIView):
+    """API view для удаления привычки"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
