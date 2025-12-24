33107b98cbed9c605d8e037d74f2de66493a2a11
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-24 18:28:14 +0300

    заполнен view файл: регистрация и удаление пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView, DestroyAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserCreateAPIView(CreateAPIView):
+    """
+    Регистрация пользователя
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class UserDeleteAPIView(DestroyAPIView):
+    """
+    Удаление пользователя
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
