e87de5edb8bf95b8729745764753953d34bfd3ca
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-24 13:30:29 +0300

    добавлена привязка к текущему пользователю при создании привычки

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,4 @@
+from django.template.context_processors import request
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from habits.models import Habit
@@ -7,8 +8,10 @@ from habits.serializers import HabitSerializer
 class HabitCreateAPIView(CreateAPIView):
     """API view для создания привычки"""
 
-    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class HabitListAPIView(ListAPIView):
