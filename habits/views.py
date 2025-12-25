0e6eff89c353a2b7ba98d6eb19241392819aa5e7
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-25 22:39:31 +0300

    добавлены условия прав доступа в crud привычек

--- habits/views.py
+++ habits/views.py
@@ -5,10 +5,12 @@ from rest_framework.generics import (
     UpdateAPIView,
     DestroyAPIView,
 )
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
 
 from habits.models import Habit
 from habits.paginations import CustomPagination
+from habits.permissions import IsOwnerOrPublicReadOnly
 from habits.serializers import HabitSerializer
 
 
@@ -16,7 +18,6 @@ class HabitCreateAPIView(CreateAPIView):
     """API view для создания привычки"""
 
     serializer_class = HabitSerializer
-    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -30,34 +31,30 @@ class HabitListAPIView(ListAPIView):
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        return Habit.objects.filter(user=self.request.user)
+        user = self.request.user
+        # свои привычки или чужие публичные
+        return Habit.objects.filter(Q(user=user) | Q(is_public=True))
 
 
 class HabitRetrieveAPIView(RetrieveAPIView):
     """API view для получения детальной информации о привычке"""
 
+    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Habit.objects.filter(user=self.request.user)
+    permission_classes = [IsAuthenticated, IsOwnerOrPublicReadOnly]
 
 
 class HabitUpdateAPIView(UpdateAPIView):
     """API view для обновления привычки"""
 
+    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Habit.objects.filter(user=self.request.user)
+    permission_classes = [IsAuthenticated, IsOwnerOrPublicReadOnly]
 
 
 class HabitDestroyAPIView(DestroyAPIView):
     """API view для удаления привычки"""
 
+    queryset = Habit.objects.all()
     serializer_class = HabitSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Habit.objects.filter(user=self.request.user)
+    permission_classes = [IsAuthenticated, IsOwnerOrPublicReadOnly]
