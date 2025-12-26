4392550ddb0f0c67121a796c848c67c2732b952f
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-26 19:01:26 +0300

    добавлены настройки rest_framework для прав доступа и аутентификации

--- config/settings.py
+++ config/settings.py
@@ -105,3 +105,12 @@ STATIC_URL = "static/"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
