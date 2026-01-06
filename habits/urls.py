6a17589e61de229b7c04082b6846eaee54982d20
Tatiana Pitieva <tatyane.liss@gmail.com>
2026-01-06 14:27:13 +0300

    добавлен путь для просмотра страницы с публичными привычками

--- habits/urls.py
+++ habits/urls.py
@@ -6,6 +6,7 @@ from habits.views import (
     HabitRetrieveAPIView,
     HabitUpdateAPIView,
     HabitDestroyAPIView,
+    PublicHabitListAPIView,
 )
 
 app_name = "habits"
@@ -14,6 +15,7 @@ app_name = "habits"
 urlpatterns = [
     path("create/", HabitCreateAPIView.as_view(), name="habit_create"),
     path("", HabitListAPIView.as_view(), name="habit_list"),
+    path("public/", PublicHabitListAPIView.as_view(), name="public_habit_list"),
     path("<int:pk>/", HabitRetrieveAPIView.as_view(), name="habit_detail"),
     path("<int:pk>/update/", HabitUpdateAPIView.as_view(), name="habit_update"),
     path("<int:pk>/delete/", HabitDestroyAPIView.as_view(), name="habit_delete"),
